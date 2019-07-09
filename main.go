10fc9743be6d0a78a05e88c6ff896db0564a1393
Mikita Kryvitski <solairerove@gmail.com>
2019-07-09 19:43:40 +0300

    feature: just init new repo

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	s "github.com/solairerove/go-struct-playground/structs"
+)
+
+func main() {
+	fmt.Println("struct examples")
+
+	s.PersonStruct()
+}
---
+++ structs/person.go
@@ -0,0 +1,8 @@
+package structs
+
+import "fmt"
+
+// PersonStruct tbd
+func PersonStruct() {
+	fmt.Println("PersonStruct")
+}
