404b7e68551a2bfd4c62ab95b318bcc765d972b0
Mikita Kryvitski <solairerove@gmail.com>
2019-07-09 19:54:36 +0300

    feature: add struct and embedded as well

--- structs/person.go
+++ structs/person.go
@@ -2,7 +2,47 @@ package structs
 
 import "fmt"
 
+type person struct {
+	name string
+	age  int
+}
+
+type secretAgent struct {
+	person
+	ltk bool
+}
+
 // PersonStruct tbd
 func PersonStruct() {
 	fmt.Println("PersonStruct")
+
+	p1 := person{
+		name: "james",
+		age:  32,
+	}
+
+	fmt.Println(p1)
+
+	p2 := person{
+		name: "mikita",
+		age:  23,
+	}
+
+	fmt.Println(p2)
+
+	people := []person{p1, p2}
+
+	fmt.Println(people)
+
+	agent := secretAgent{
+		person: person{
+			name: "James Bond",
+			age:  32,
+		},
+		ltk: true,
+	}
+
+	fmt.Println("embedded struct")
+	fmt.Println(agent)
+	fmt.Println(agent.ltk, agent.name)
 }
